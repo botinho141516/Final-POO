a00fba43fbe19608271bb26606c6213e9f44afb6
ringoball <jonandovb@hotmail.com>
2017-11-17 19:41:04 -0200

    Vê aí

--- TrabalhoFinal/src/Controles/ControleAssociado.java
+++ TrabalhoFinal/src/Controles/ControleAssociado.java
@@ -1,20 +1,56 @@
 package Controles;
 
+import Entidades.Associado;
 import Visões.ViewAssociado;
-
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Vector;
 
 public class ControleAssociado {
-    
+
     ViewAssociado vA;
-    
-    public ControleAssociado(ViewAssociado view)
-    {
+    private Vector<Associado> Associados = new Vector();
+
+    public ControleAssociado(ViewAssociado view) {
         vA = view;
+        es();
     }
-    
+
+    public void serializaAssociados() {
+        try {
+            FileOutputStream objFileOS = new FileOutputStream("Associados.dat");
+            ObjectOutputStream objOS = new ObjectOutputStream(objFileOS);
+            objOS.writeObject(Associados);
+            objOS.flush();
+            objOS.close();
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void es() {
+
+        try {
+            File objFile = new File("Associados.dat");
+            if (objFile.exists()) {
+                FileInputStream objFileIS = new FileInputStream("Associados.dat");
+                ObjectInputStream objIS = new ObjectInputStream(objFileIS);
+                Associados = (Vector) objIS.readObject();
+                //System.out.println(lista.size());
+
+                objIS.close();
+            }
+             } catch (Exception e) {
+
+        }
+    }
+
     public void cadastraAsssociado(int id, String text, String text0, String text1, String toString) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Associados.add(new Associado(id, text, text0, text1, toString));
+        serializaAssociados();
     }
 
 }
--- TrabalhoFinal/src/Controles/ControlePublicacao.java
+++ TrabalhoFinal/src/Controles/ControlePublicacao.java
@@ -1,20 +1,57 @@
 package Controles;
 
+import Entidades.Publicacao;
 import Visões.ViewPublicacao;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Vector;
 
 
 
 public class ControlePublicacao {
     
     ViewPublicacao vP;
-    
+    private Vector<Publicacao> Publicacoes = new Vector();
+     
     public ControlePublicacao(ViewPublicacao view)
     {
         vP = view;
+        es();
     }
 
+     public void serializaPublicacoes() {
+        try {
+            FileOutputStream objFileOS = new FileOutputStream("Publicacoes.dat");
+            ObjectOutputStream objOS = new ObjectOutputStream(objFileOS);
+            objOS.writeObject(Publicacoes);
+            objOS.flush();
+            objOS.close();
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void es() {
+
+        try {
+            File objFile = new File("Publicacoes.dat");
+            if (objFile.exists()) {
+                FileInputStream objFileIS = new FileInputStream("Publicacoes.dat");
+                ObjectInputStream objIS = new ObjectInputStream(objFileIS);
+                Publicacoes = (Vector) objIS.readObject();
+                objIS.close();
+            }
+             } catch (Exception e) {
+
+        }
+    }
+    
     public void cadastraPublicacao(int id, String text, String text0, String text1, String text2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Publicacoes.add(new Publicacao(id, text, text0, text1, text2));
+        serializaPublicacoes();
     }
 
 }
